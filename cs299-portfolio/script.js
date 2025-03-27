98a64d6ca3910fe2f8422da0cf35a97a0acc1de5
karringtonwilson <karringtonswilson@gmail.com>
2025-03-26 23:55:31 -0400

    Landing pafe for cs299 portfolio

---
+++ cs299-portfolio/script.js
@@ -0,0 +1,10 @@
+function buttonClicked() {
+    document.getElementById("buttonplay").play();
+    document.getElementById("welcome-page").style.display = "none";
+    // Show the second page
+    document.getElementById("main-content").style.display = "block";
+}
+
+function linkPlay() {
+    document.getElementById("linkplay").play();
+}
